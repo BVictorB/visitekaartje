485d089959fe163c917f7799237d2e8b856b0cf1
bvictorb <victory-boucher@hotmail.com>
2021-02-02 10:31:38 +0100

    added put function to send data to the api

--- js/app.js
+++ js/app.js
@@ -1,28 +1,35 @@
-// const xhr = new XMLHttpRequest()
-// const url = 'https://600ff44f6c21e1001704fac2.mockapi.io/minor-web/api/squads/1/teams/1/members'
+const url = 'https://600ff44f6c21e1001704fac2.mockapi.io/minor-web/api/squads/1/teams/1/members/7'
+const userData = {
+  "teamId": "1",
+  "name": "Victor",
+  "avatar": "https://avatars.githubusercontent.com/u/10921830?s=460&u=d9790f50bebe35de86146b7fcb76906286611703&v=4",
+  "prefix": "",
+  "surname": "Boucher",
+  "mugshot": "https://avatars.githubusercontent.com/u/10921830?s=460&u=d9790f50bebe35de86146b7fcb76906286611703&v=4",
+  "githubHandle": "BVictorB",
+  "other": {
+    "age": "22",
+    "sport": "(kick)boksen, wielrennen, voetballen, krachttraining",
+    "pet": "Katten",
+    "muziek": "Hip-hop/rap, Drum & Bass, Lo-fi, en nog veel meer",
+    "werkplek": "Thuis"
+  }
+}
 
-// const userData = {
-//   "teamId": "1",
-//   "name": "Victor",
-//   "avatar": "https://avatars.githubusercontent.com/u/10921830?s=460&u=d9790f50bebe35de86146b7fcb76906286611703&v=4",
-//   "prefix": "",
-//   "surname": "Boucher",
-//   "mugshot": "https://avatars.githubusercontent.com/u/10921830?s=460&u=d9790f50bebe35de86146b7fcb76906286611703&v=4",
-//   "githubHandle": "BVictorB",
-//   "other": {
-//     "age": "22",
-//     "sport": "(kick)boksen, wielrennen, voetballen, krachttraining",
-//     "pet": "Katten",
-//     "muziek": "Hip-hop/rap, Drum & Bass, Lo-fi, en nog veel meer",
-//     "werkplek": "Thuis"
-//   }
-// }
-
-// const postUserData = () => {
-//   xhr.open("POST", url, true)
-//   xhr.setRequestHeader('Content-Type', 'application/json')
-//   xhr.send(JSON.stringify(userData))
-// }
+fetch(url, {
+  method: 'PUT',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(userData),
+})
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data)
+  })
+  .catch((error) => {
+    console.error('Error:', error)
+  })
 
 const 
   card = document.querySelector('.card'),
